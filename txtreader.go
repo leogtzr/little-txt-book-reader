3cb996eb056032ddbd8fb4da10a18736cac446d2
leogtzr <leogutierrezramirez@gmail.com>
2019-08-09 22:59:27 -0600

    add some refactoring

--- txtreader.go
+++ txtreader.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -10,9 +9,7 @@ import (
 	"os"
 	"path"
 	"path/filepath"
-	"regexp"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/marcusolsson/tui-go"
@@ -35,61 +32,13 @@ var gotoLine = ""
 var fileToOpen = flag.String("file", "", "File to open")
 var openLatestFile = flag.Bool("latest", false, "Open the latest text file")
 var percentagePointStats = false
+var absoluteFilePath string
 
 // LatestFile ...
 type LatestFile struct {
 	FileName string
 	From     int
 	To       int
-}
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
-// readLines reads a whole file into memory
-// and returns a slice of its lines.
-func readLines(path string) ([]string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
-}
-
-func getChunk(fileContent *[]string, from, to int) []string {
-	return (*fileContent)[from:to]
-}
-
-func clearBox(box *tui.Box, contentLen int) {
-	for i := 0; i < contentLen; i++ {
-		box.Append(tui.NewHBox(
-			tui.NewLabel(""),
-			tui.NewSpacer(),
-		))
-	}
-}
-
-func putText(box *tui.Box, content *[]string) {
-	clearBox(box, len(*content))
-	for _, txt := range *content {
-		txt = strings.Replace(txt, " ", " ", -1)
-		txt = strings.Replace(txt, "\t", "    ", -1)
-		txt = wrap(txt)
-		box.Append(tui.NewVBox(
-			tui.NewLabel(txt),
-			tui.NewSpacer(),
-		))
-	}
 }
 
 func downText(fileContent *[]string, txtArea *tui.Box) {
@@ -122,36 +71,6 @@ func upText(fileContent *[]string, txtArea *tui.Box) {
 	putText(txtArea, &chunk)
 }
 
-func needsSemiWrap(line string) bool {
-	len := len(line)
-	if len < (WrapMax / 2) {
-		return false
-	}
-	return (len > (WrapMax / 2)) && (len < WrapMax)
-}
-
-func countWithoutWhitespaces(words []string) int {
-	count := 0
-	for _, w := range words {
-		count += len(w)
-	}
-	return count
-}
-
-func wrap(line string) string {
-	if !needsSemiWrap(line) {
-		return line
-	}
-	fields := strings.Fields(line)
-	numberOfWords := len(fields)
-	countWithoutSpaces := countWithoutWhitespaces(fields)
-	wrapLength := WrapMax - countWithoutSpaces
-	if numberOfWords == 1 || numberOfWords == 0 {
-		return line
-	}
-	return fmt.Sprintf("%s", strings.Join(fields, strings.Repeat(" ", wrapLength/(numberOfWords-1))))
-}
-
 func getStatusInformation(fileContent *[]string) string {
 	percent := float64(to) * 100.00
 	percent = percent / float64(len(*fileContent))
@@ -164,120 +83,6 @@ func getStatusInformation(fileContent *[]string) string {
 		to, len(*fileContent), percent)
 
 }
-
-func addUpBinding(fileContent *[]string, box *tui.Box, input *tui.Entry) func() {
-	return func() {
-		upText(fileContent, box)
-		input.SetText(getStatusInformation(fileContent))
-	}
-}
-
-func addDownBinding(fileContent *[]string, box *tui.Box, input *tui.Entry) func() {
-	return func() {
-		downText(fileContent, box)
-		input.SetText(getStatusInformation(fileContent))
-	}
-}
-
-func addGotoWidget(box *tui.Box) {
-	gotoInput := tui.NewTextEdit()
-	gotoInput.SetText("Go to line: ")
-	gotoInput.SetFocused(true)
-	gotoInput.OnTextChanged(func(entry *tui.TextEdit) {
-		gotoLine = entry.Text()
-	})
-	box.Append(gotoInput)
-}
-
-func exists(name string) bool {
-	if _, err := os.Stat(name); os.IsNotExist(err) {
-		return false
-	}
-	return true
-}
-
-func saveStatus(fileName string, from, to int) {
-	// write from, to y el nombre del archivo ...
-	homeDir := os.Getenv("HOME")
-	f, err := os.Create(filepath.Join(homeDir, "txtread"))
-	if err != nil {
-		fmt.Fprint(os.Stderr, err.Error())
-		return
-	}
-	defer f.Close()
-	f.WriteString(fmt.Sprintf("%s|%d|%d", fileName, from, to))
-}
-
-func getFileNameFromLatest() (LatestFile, error) {
-	homeDir := os.Getenv("HOME")
-	latestFilePath := filepath.Join(homeDir, "txtread")
-
-	if !exists(latestFilePath) {
-		return LatestFile{}, fmt.Errorf("'%s' does not exist", latestFilePath)
-	}
-
-	f, err := os.Open(latestFilePath)
-	if err != nil {
-		return LatestFile{}, err
-	}
-	defer f.Close()
-	fileContent, err := ioutil.ReadAll(f)
-	if err != nil {
-		return LatestFile{}, err
-	}
-	latestFileFields := strings.Split(string(fileContent), "|")
-	if len(latestFileFields) != 3 {
-		return LatestFile{}, fmt.Errorf("Wrong format in '%s'", latestFilePath)
-	}
-	latestFile := LatestFile{}
-	latestFile.FileName = latestFileFields[0]
-	fromInt, _ := strconv.ParseInt(latestFileFields[1], 10, 32)
-	toInt, _ := strconv.ParseInt(latestFileFields[2], 10, 32)
-
-	latestFile.From = int(fromInt)
-	latestFile.To = int(toInt)
-	return latestFile, nil
-}
-
-func getNumberLineGoto(line string) string {
-	reg, err := regexp.Compile("[^0-9]+")
-	if err != nil {
-		return ""
-	}
-	return reg.ReplaceAllString(line, "")
-}
-
-func percent(i, totalLines int) float64 {
-	return float64(i*100.0) / float64(totalLines)
-}
-
-func linesToChangePercentagePoint(currentLine, totalLines int) int {
-	start := currentLine
-	linesToChangePercentage := -1
-	percentageWithCurrentLine := int(percent(currentLine, totalLines))
-	for {
-		currentLine++
-		nextPercentage := int(percent(currentLine, totalLines))
-		if nextPercentage > percentageWithCurrentLine {
-			linesToChangePercentage = currentLine
-			break
-		}
-	}
-
-	return linesToChangePercentage - start
-}
-
-func dirExists(dirPath string) bool {
-	if _, err := os.Stat(dirPath); err != nil {
-		if os.IsNotExist(err) {
-			return false
-		}
-		return false
-	}
-	return true
-}
-
-var absoluteFilePath string
 
 func main() {
 
@@ -350,34 +155,33 @@ func main() {
 	}
 
 	// down ...
-	ui.SetKeybinding("j", addDownBinding(&fileContent, txtArea, inputCommand))
-	ui.SetKeybinding("Down", addDownBinding(&fileContent, txtArea, inputCommand))
+	ui.SetKeybinding(downKeyBindingAlternative1, addDownBinding(&fileContent, txtArea, inputCommand))
+	ui.SetKeybinding(downKeyBindingAlternative2, addDownBinding(&fileContent, txtArea, inputCommand))
 
 	// Up ...
-	ui.SetKeybinding("k", addUpBinding(&fileContent, txtArea, inputCommand))
-	ui.SetKeybinding("Up", addUpBinding(&fileContent, txtArea, inputCommand))
+	ui.SetKeybinding(upKeyBindingAlternative1, addUpBinding(&fileContent, txtArea, inputCommand))
+	ui.SetKeybinding(upKeyBindingAlternative2, addUpBinding(&fileContent, txtArea, inputCommand))
 
 	// go to:
-	ui.SetKeybinding("Alt+g", func() {
+	ui.SetKeybinding(gotoKeyBindingAlterntive1, func() {
 		addGotoWidget(txtReader)
 	})
 
 	noteBox := tui.NewTextEdit()
 	noteBox.SetText("")
 
-	ui.SetKeybinding("Alt+n", func() {
-		noteBox.SetText("")
-		noteBox.SetSizePolicy(tui.Expanding, tui.Expanding)
-		noteBox.SetFocused(true)
+	ui.SetKeybinding(newNoteKeyBindingAlternative1, func() {
+		prepareNewNoteBox(noteBox)
 		inputCommand.SetFocused(false)
 		inputCommand.SetText("> > > > > Creating note ... ")
-		noteBox.SetWordWrap(true)
 		txtReader.SetFocused(false)
+		txtArea.SetFocused(false)
+		txtAreaScroll.SetFocused(false)
 
 		txtReader.Insert(0, noteBox)
 	})
 
-	ui.SetKeybinding("Alt+s", func() {
+	ui.SetKeybinding(saveNoteKeyBindingAlternative1, func() {
 
 		if !noteBox.IsFocused() {
 			return
@@ -401,7 +205,7 @@ func main() {
 		txtReader.Remove(0)
 	})
 
-	ui.SetKeybinding("r", func() {
+	ui.SetKeybinding(closeGotoKeyBindingAlternative1, func() {
 		// Go to the specified line
 		inputCommand.SetText(getStatusInformation(&fileContent))
 
@@ -419,19 +223,15 @@ func main() {
 		txtReader.Remove(GotoWidgetIndex)
 	})
 
-	ui.SetKeybinding("s", func() {
-		// save status ...
-		absoluteFilePath, _ := filepath.Abs(fileName)
-		saveStatus(absoluteFilePath, from, to)
-	})
+	ui.SetKeybinding(saveStatusKeyBindingAlternative1, saveReadingStatus(fileName, from, to))
 
 	// Enable percentage stags
-	ui.SetKeybinding("Alt+p", func() {
+	ui.SetKeybinding(nextPercentagePointKeyBindingAlternative1, func() {
 		percentagePointStats = !percentagePointStats
 		inputCommand.SetText(getStatusInformation(&fileContent))
 	})
 
-	ui.SetKeybinding("Esc", func() {
+	ui.SetKeybinding(closeApplicationKeyBindingAlterntive1, func() {
 		ui.Quit()
 	})
 
---
+++ utils.go
@@ -0,0 +1,231 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/marcusolsson/tui-go"
+)
+
+const downKeyBindingAlternative1 = "Alt+j"
+const downKeyBindingAlternative2 = "Down"
+const upKeyBindingAlternative1 = "Alt+k"
+const upKeyBindingAlternative2 = "Up"
+const gotoKeyBindingAlterntive1 = "Alt+g"
+const newNoteKeyBindingAlternative1 = "Alt+n"
+const saveNoteKeyBindingAlternative1 = "Alt+s"
+const closeGotoKeyBindingAlternative1 = "r"
+const saveStatusKeyBindingAlternative1 = "s"
+const nextPercentagePointKeyBindingAlternative1 = "Alt+p"
+const closeApplicationKeyBindingAlterntive1 = "Esc"
+
+func prepareNewNoteBox(noteBox *tui.TextEdit) {
+	noteBox.SetText("")
+	noteBox.SetSizePolicy(tui.Expanding, tui.Expanding)
+	noteBox.SetFocused(true)
+	noteBox.SetWordWrap(true)
+}
+
+func saveStatus(fileName string, from, to int) {
+	// write from, to y el nombre del archivo ...
+	homeDir := os.Getenv("HOME")
+	f, err := os.Create(filepath.Join(homeDir, "txtread"))
+	if err != nil {
+		fmt.Fprint(os.Stderr, err.Error())
+		return
+	}
+	defer f.Close()
+	f.WriteString(fmt.Sprintf("%s|%d|%d", fileName, from, to))
+}
+
+func saveReadingStatus(fileName string, from, to int) func() {
+	return func() {
+		absoluteFilePath, _ := filepath.Abs(fileName)
+		saveStatus(absoluteFilePath, from, to)
+	}
+}
+
+func getNumberLineGoto(line string) string {
+	reg, err := regexp.Compile("[^0-9]+")
+	if err != nil {
+		return ""
+	}
+	return reg.ReplaceAllString(line, "")
+}
+
+func percent(i, totalLines int) float64 {
+	return float64(i*100.0) / float64(totalLines)
+}
+
+func linesToChangePercentagePoint(currentLine, totalLines int) int {
+	start := currentLine
+	linesToChangePercentage := -1
+	percentageWithCurrentLine := int(percent(currentLine, totalLines))
+	for {
+		currentLine++
+		nextPercentage := int(percent(currentLine, totalLines))
+		if nextPercentage > percentageWithCurrentLine {
+			linesToChangePercentage = currentLine
+			break
+		}
+	}
+
+	return linesToChangePercentage - start
+}
+
+func getFileNameFromLatest() (LatestFile, error) {
+	homeDir := os.Getenv("HOME")
+	latestFilePath := filepath.Join(homeDir, "txtread")
+
+	if !exists(latestFilePath) {
+		return LatestFile{}, fmt.Errorf("'%s' does not exist", latestFilePath)
+	}
+
+	f, err := os.Open(latestFilePath)
+	if err != nil {
+		return LatestFile{}, err
+	}
+	defer f.Close()
+	fileContent, err := ioutil.ReadAll(f)
+	if err != nil {
+		return LatestFile{}, err
+	}
+	latestFileFields := strings.Split(string(fileContent), "|")
+	if len(latestFileFields) != 3 {
+		return LatestFile{}, fmt.Errorf("Wrong format in '%s'", latestFilePath)
+	}
+	latestFile := LatestFile{}
+	latestFile.FileName = latestFileFields[0]
+	fromInt, _ := strconv.ParseInt(latestFileFields[1], 10, 32)
+	toInt, _ := strconv.ParseInt(latestFileFields[2], 10, 32)
+
+	latestFile.From = int(fromInt)
+	latestFile.To = int(toInt)
+	return latestFile, nil
+}
+
+func dirExists(dirPath string) bool {
+	if _, err := os.Stat(dirPath); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+		return false
+	}
+	return true
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func getChunk(fileContent *[]string, from, to int) []string {
+	return (*fileContent)[from:to]
+}
+
+func clearBox(box *tui.Box, contentLen int) {
+	for i := 0; i < contentLen; i++ {
+		box.Append(tui.NewHBox(
+			tui.NewLabel(""),
+			tui.NewSpacer(),
+		))
+	}
+}
+
+func needsSemiWrap(line string) bool {
+	len := len(line)
+	if len < (WrapMax / 2) {
+		return false
+	}
+	return (len > (WrapMax / 2)) && (len < WrapMax)
+}
+
+func countWithoutWhitespaces(words []string) int {
+	count := 0
+	for _, w := range words {
+		count += len(w)
+	}
+	return count
+}
+
+func wrap(line string) string {
+	if !needsSemiWrap(line) {
+		return line
+	}
+	fields := strings.Fields(line)
+	numberOfWords := len(fields)
+	countWithoutSpaces := countWithoutWhitespaces(fields)
+	wrapLength := WrapMax - countWithoutSpaces
+	if numberOfWords == 1 || numberOfWords == 0 {
+		return line
+	}
+	return fmt.Sprintf("%s", strings.Join(fields, strings.Repeat(" ", wrapLength/(numberOfWords-1))))
+}
+
+func addGotoWidget(box *tui.Box) {
+	gotoInput := tui.NewTextEdit()
+	gotoInput.SetText("Go to line: ")
+	gotoInput.SetFocused(true)
+	gotoInput.OnTextChanged(func(entry *tui.TextEdit) {
+		gotoLine = entry.Text()
+	})
+	box.Append(gotoInput)
+}
+
+func exists(name string) bool {
+	if _, err := os.Stat(name); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+func addUpBinding(fileContent *[]string, box *tui.Box, input *tui.Entry) func() {
+	return func() {
+		upText(fileContent, box)
+		input.SetText(getStatusInformation(fileContent))
+	}
+}
+
+func addDownBinding(fileContent *[]string, box *tui.Box, input *tui.Entry) func() {
+	return func() {
+		downText(fileContent, box)
+		input.SetText(getStatusInformation(fileContent))
+	}
+}
+
+func putText(box *tui.Box, content *[]string) {
+	clearBox(box, len(*content))
+	for _, txt := range *content {
+		txt = strings.Replace(txt, " ", " ", -1)
+		txt = strings.Replace(txt, "\t", "    ", -1)
+		txt = wrap(txt)
+		box.Append(tui.NewVBox(
+			tui.NewLabel(txt),
+			tui.NewSpacer(),
+		))
+	}
+}

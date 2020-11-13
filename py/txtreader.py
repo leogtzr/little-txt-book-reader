e49d9b47f68f0dde90aa86015a7a895113192b45
leogtzr <leogutierrezramirez@gmail.com>
2020-11-12 21:45:03 -0700

    viewing txt files with key bindings

--- py/txtreader.py
+++ py/txtreader.py
@@ -1,5 +1,4 @@
 import curses
-from curses import textpad
 import sys
 from sys import stderr
 
@@ -8,10 +7,87 @@ if len(sys.argv) != 2:
 
 filename = sys.argv[1]
 
-try:
-    with open(filename, 'r') as f:
-        lines = [line.rstrip('\n') for line in f.readlines()]
-except FileNotFoundError:
-    sys.exit(f"error: file not found: {filename}\n")
-else:
-    print(lines)
+# The following constant is "temporary", ideally it should be calculated based:
+ADVANCE = 30
+KEY_ESCAPE_CODE = 27
+
+# book_page could probably a slice.
+
+
+def book_chunk(lines, from_line, to_line, book_number_of_lines):
+    return lines[from_line:to_line]
+
+
+def print_page(stdscr, selected_row_idx, book_page):
+    for idx, book_page_line in enumerate(book_page):
+        # Optional code:
+        if idx == selected_row_idx:
+            stdscr.attron(curses.color_pair(1))
+            stdscr.addstr(idx, 0, book_page_line)
+            stdscr.attroff(curses.color_pair(1))
+        else:
+            stdscr.addstr(idx, 0, book_page_line)
+
+
+def main(stdscr):
+    try:
+        with open(filename, 'r') as f:
+            lines = [line.rstrip('\n') for line in f.readlines()]
+    except FileNotFoundError:
+        sys.exit(f"error: file not found: {filename}\n")
+    else:
+        curses.curs_set(0)
+        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
+        book_number_of_lines = len(lines)
+
+        MAX_HEIGHT, _ = stdscr.getmaxyx()
+        ADVANCE = MAX_HEIGHT
+        from_line = 0
+        to_line = ADVANCE
+        current_row = ADVANCE//2
+
+        book_page = book_chunk(lines, from_line, to_line, book_number_of_lines)
+        print_page(stdscr, current_row, book_page)
+
+        # Loop
+        while True:
+            key = stdscr.getch()
+            stdscr.clear()
+
+            if key in [KEY_ESCAPE_CODE]:                # exit
+                stdscr.refresh()
+                sys.exit(0)
+
+            elif key == curses.KEY_UP:
+                # from_line -= 1
+                current_row -= 1
+
+            elif key == curses.KEY_DOWN:
+                if current_row >= (MAX_HEIGHT - 2):
+                    # current_row -= 1
+                    # current_row = 0
+                    # current_row += 1
+                    # from_line += ADVANCE
+                    # to_line = from_line + ADVANCE
+                    from_line += 1
+                    to_line += 1
+                    stdscr.clear()
+                else:
+                    current_row += 1
+                # to_line += 1
+
+            stdscr.refresh()
+
+            # Update status bar:
+            book_page = book_chunk(
+                lines, from_line, to_line-1, book_number_of_lines)
+            print_page(stdscr, current_row, book_page)
+            stdscr.addstr(MAX_HEIGHT - 1, 0,
+                          f"Current line: {current_row}, max: {MAX_HEIGHT}")
+
+
+curses.wrapper(main)
+
+# TODO:
+# current_row will be the one that be highlighted.

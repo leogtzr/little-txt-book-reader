219528f8129a259186d84729567a2400ae081e9e
leogtzr <leogutierrezramirez@gmail.com>
2020-11-13 21:31:40 -0700

    remove magic numbers and remove debug text from status bar

--- py/txtreader.py
+++ py/txtreader.py
@@ -10,6 +10,7 @@ filename = sys.argv[1]
 # The following constant is "temporary", ideally it should be calculated based:
 # ADVANCE = 30
 KEY_ESCAPE_CODE = 27
+HIGHLIGHT_COLOR_PAIRCODE = 1
 STATUSBAR_COLOR_PAIRCODE = 2
 
 # book_page could probably a slice.
@@ -22,9 +23,9 @@ def book_chunk(lines, from_line, to_line, book_number_of_lines):
 def print_page(stdscr, selected_row_idx, book_page):
     for idx, book_page_line in enumerate(book_page):
         if idx == selected_row_idx:
-            stdscr.attron(curses.color_pair(1))
+            stdscr.attron(curses.color_pair(HIGHLIGHT_COLOR_PAIRCODE))
             stdscr.addstr(idx, 0, book_page_line)
-            stdscr.attroff(curses.color_pair(1))
+            stdscr.attroff(curses.color_pair(HIGHLIGHT_COLOR_PAIRCODE))
         else:
             stdscr.addstr(idx, 0, book_page_line)
 
@@ -43,7 +44,8 @@ def main(stdscr):
         sys.exit(f"error: file not found: {filename}\n")
     else:
         curses.curs_set(0)
-        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+        curses.init_pair(HIGHLIGHT_COLOR_PAIRCODE,
+                         curses.COLOR_BLACK, curses.COLOR_WHITE)
         curses.init_pair(STATUSBAR_COLOR_PAIRCODE,
                          curses.COLOR_BLACK, curses.COLOR_GREEN)
 
@@ -59,36 +61,34 @@ def main(stdscr):
         book_page = book_chunk(lines, from_line, to_line, book_number_of_lines)
         print_page(stdscr, current_row, book_page)
         print_status_bar(
-            stdscr, MAX_HEIGHT - 1, f"Current line: {line_number}, from_line: {from_line}, to_line: {to_line}")
+            stdscr, MAX_HEIGHT - 1, f"Current line: {line_number}")
 
         # Loop
         while True:
             key = stdscr.getch()
             stdscr.clear()
 
-            if key in [KEY_ESCAPE_CODE]:                # exit
+            if key in [KEY_ESCAPE_CODE]:
                 stdscr.refresh()
                 sys.exit(0)
 
             elif key == curses.KEY_UP:
                 if line_number > 1:
-                    line_number -= 1
                     current_row -= 1
                 if from_line > 0:
                     from_line -= 1
                     to_line -= 1
-                    line_number -= 1
+                line_number -= 1
 
             elif key == curses.KEY_DOWN:
                 if line_number < book_number_of_lines:
                     if current_row >= (MAX_HEIGHT - 2):
                         from_line += 1
                         to_line += 1
-                        line_number += 1
                         stdscr.clear()
                     else:
                         current_row += 1
-                        line_number += 1
+                    line_number += 1
 
             stdscr.refresh()
 
@@ -97,7 +97,7 @@ def main(stdscr):
                 lines, from_line, to_line-1, book_number_of_lines)
             print_page(stdscr, current_row, book_page)
             print_status_bar(
-                stdscr, MAX_HEIGHT - 1, f"Current line: {line_number}, from_line: {from_line}, to_line: {to_line}")
+                stdscr, MAX_HEIGHT - 1, f"Current line: {line_number}")
 
 
 curses.wrapper(main)

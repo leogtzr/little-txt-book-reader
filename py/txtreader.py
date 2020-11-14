adf4216848f15d0ebba9435c399e6fcb26cedf01
leogtzr <leogutierrezramirez@gmail.com>
2020-11-13 22:05:57 -0700

    show textpad for help

--- py/txtreader.py
+++ py/txtreader.py
@@ -1,6 +1,14 @@
 import curses
+from curses import textpad
 import sys
 from sys import stderr
+from enum import Enum
+
+
+class WindowMode(Enum):
+    reading = 1
+    help = 2
+
 
 if len(sys.argv) != 2:
     sys.exit(1)
@@ -45,14 +53,13 @@ def main(stdscr):
                          curses.COLOR_BLACK, curses.COLOR_WHITE)
         curses.init_pair(STATUSBAR_COLOR_PAIRCODE,
                          curses.COLOR_BLACK, curses.COLOR_GREEN)
-
         book_number_of_lines = len(lines)
-
-        MAX_HEIGHT, _ = stdscr.getmaxyx()
+        MAX_HEIGHT, MAX_WIDTH = stdscr.getmaxyx()
         from_line = 0
         to_line = MAX_HEIGHT
         current_row = 0
         line_number = 1
+        window_mode = WindowMode.reading
 
         book_page = book_chunk(lines, from_line, to_line, book_number_of_lines)
         print_page(stdscr, current_row, book_page)
@@ -65,8 +72,20 @@ def main(stdscr):
             stdscr.clear()
 
             if key in [KEY_ESCAPE_CODE]:
-                stdscr.refresh()
-                sys.exit(0)
+                if window_mode == WindowMode.help:
+                    window_mode = WindowMode.reading
+                elif window_mode == WindowMode.reading:
+                    stdscr.refresh()
+                    sys.exit(0)
+
+            elif key in [72, 104]:
+                window_mode = WindowMode.help
+                stdscr.clear()
+
+                sh, sw = stdscr.getmaxyx()
+                box = [[3, 3], [sh-3, sw-3]]
+                textpad.rectangle(
+                    stdscr, box[0][0], box[0][1], box[1][0], box[1][1])
 
             elif key == curses.KEY_UP:
                 if line_number > 1:
@@ -88,11 +107,12 @@ def main(stdscr):
 
             stdscr.refresh()
 
-            book_page = book_chunk(
-                lines, from_line, to_line-1, book_number_of_lines)
-            print_page(stdscr, current_row, book_page)
-            print_status_bar(
-                stdscr, MAX_HEIGHT - 1, f"Current line: {line_number}")
+            if window_mode == WindowMode.reading:
+                book_page = book_chunk(
+                    lines, from_line, to_line-1, book_number_of_lines)
+                print_page(stdscr, current_row, book_page)
+                print_status_bar(
+                    stdscr, MAX_HEIGHT - 1, f"Current line: {line_number}")
 
 
 curses.wrapper(main)
